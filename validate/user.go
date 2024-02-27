654335e5bd0e7d20469cba8214047fd04dc83c53
ppengryuu <255499213@qq.com>
2024-02-27 16:50:34 +0800

    fix: 修复用户修改资料验证规则错误
    reason: 无

--- validate/user.go
+++ validate/user.go
@@ -43,7 +43,7 @@ func (*User) EditProfile() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		params, _ := utils.GetRequestParams[request.EditProfile](c)
 		if err := StructValidate(c, &params,
-			validation.Field(&params.Name, validation.Required, validation.Max(16)),
+			validation.Field(&params.Name, validation.Required, validation.Length(1, 16)),
 		); err != nil {
 			c.Abort()
 			return
